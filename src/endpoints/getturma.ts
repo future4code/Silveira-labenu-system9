f6d7709931c276a199a0a816b873e42de6ded146
Baulii <adernam147@hotmail.com>
2022-06-24 14:53:43 -0400

    Finalizado parte de turmas sem POO

--- src/endpoints/getturma.ts
+++ src/endpoints/getturma.ts
@@ -5,10 +5,10 @@ import { connection } from "../data/BaseDataBase";
 export default async function getTurma(req: Request, res: Response) {
     try {
         const result = await connection(`Turma`)
-            .select("nome", "modulo")
+            .select("id", "nome", "modulo");
         res.send(result);
     } catch (error) {
         console.log(error);
-        res.status(400).send("User not found.")
-    }
-}
+        res.status(400).send("User not found.");
+    };
+};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { app } from "./app";
 import createDocentes from "./endpoints/createDocentes";
+import deleteTurma from "./endpoints/deleteTurma";
+import editModulo from "./endpoints/editModulo";
 import getDocentes from "./endpoints/getDocentes";
-import getTurma from "./endpoints/getturma";
+import getTurma from "./endpoints/getTurma";
 import postTurma from "./endpoints/postTurma";
 import putDocentes from "./endpoints/putDocentes";
 
 app.post("/turma", postTurma);
 app.get("/turma", getTurma);
+app.put("/turma/:id", editModulo);
+app.delete("/turma/:id", deleteTurma);
 
 app.post("/docente", createDocentes);
 app.get("/docente", getDocentes);
